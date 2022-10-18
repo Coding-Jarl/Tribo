976b573e099a70367576a6821f31c5df097db177
ValhallaCode <senvisage@gmail.com>
2022-10-18 22:06:45 +0200

    First content+CSS pass on GameCard

--- frontend/src/components/GameCard/index.jsx
+++ frontend/src/components/GameCard/index.jsx
@@ -3,7 +3,20 @@ import Styled from "./style";
 export default function GameCard() {
   return (
     <Styled>
-      <h2>I iz a card ! </h2>
+      <div className="boxPhoto" />
+      <h2>Azul - Maitre chocolatier (2022) - Next Move Games</h2>
+      <h3>2-4 joueurs - 30-45 minutes</h3>
+      <p>
+        Azul : Maître Chocolatier, le classique de Michael Kiesling dans une
+        version thématisée pour les plus gourmands ! Joignez-vous aux meilleurs
+        fabricants de chocolat du monde dans Azul : Maître Chocolatier !
+      </p>
+      <ul>
+        <li>Alimentation</li>
+        <li>Chocolat</li>
+        <li>Combinaison</li>
+        <li>Placement</li>
+      </ul>
     </Styled>
   );
 }
--- frontend/src/components/GameCard/style.jsx
+++ frontend/src/components/GameCard/style.jsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export default styled.article`
-  color: yellowgreen;
-  font-size: 5rem;
+  border: 1px solid grey;
+  width: 20rem;
+  aspect-ratio: 3/4;
+
+  .boxPhoto {
+    background-image: url("https://www.myludo.fr/img/jeux/1665499612/300/ci/60212.png");
+    aspect-ratio: 2;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
 `;

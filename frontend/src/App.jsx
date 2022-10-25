c4e9feeca2f089e14bde15a3b477c49a337aeee8
ValhallaCode <senvisage@gmail.com>
2022-10-25 09:07:30 +0200

    Add styling to GameCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import GameCard from "@components/GameCard";
 export default function App() {
   return (
     <>
-      <h1>Hi Tribo!</h1>
+      Tribo!
       <GameCard />
     </>
   );
--- frontend/src/components/GameCard/style.jsx
+++ frontend/src/components/GameCard/style.jsx
@@ -1,15 +1,40 @@
 import styled from "styled-components";
 
 export default styled.article`
-  border: 1px solid grey;
-  width: 20rem;
-  aspect-ratio: 3/4;
+  position: relative;
+  width: 15rem;
+  aspect-ratio: 2.5;
+  margin: auto;
 
-  .boxPhoto {
-    background-image: url("https://www.myludo.fr/img/jeux/1665499612/300/ci/60212.png");
-    aspect-ratio: 2;
-    background-repeat: no-repeat;
-    background-position: center;
+  & > main {
+    position: absolute;
+    opacity: 0;
+    transition: 0.2s opacity ease-in-out 0.25s;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    background-color: white;
+
+    width: 125%;
+    aspect-ratio: 3/4;
+    top: 0;
+    left: -12.5%;
+  }
+
+  &:hover > main {
+    opacity: 1;
+  }
+
+  aside {
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(
+        0deg,
+        rgba(0, 0, 0, 0.4) 0%,
+        rgba(0, 0, 0, 0) 25%,
+        rgba(0, 0, 0, 0) 75%,
+        rgba(0, 0, 0, 0.4) 100%
+      ),
+      url("${(props) => props.rawData.thumb}");
     background-size: cover;
+    background-position: center;
   }
 `;

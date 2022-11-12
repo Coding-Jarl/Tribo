53b31093226584f9fb4d5bc060b83299872dc5f0
Coding-Jarl <senvisage@gmail.com>
2022-11-12 14:55:42 +0100

    Add simplist -but effective- style to GameCard

--- frontend/src/components/GameCard/style.jsx
+++ frontend/src/components/GameCard/style.jsx
@@ -4,23 +4,22 @@ export default styled.article`
   position: relative;
   width: 15rem;
   aspect-ratio: 2.5;
-  margin: auto;
+  margin: 4rem;
 
   & > main {
     position: absolute;
-    opacity: 0;
-    transition: 0.2s opacity ease-in-out 0.25s;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    background-color: white;
-
+    left: -12.5%;
+    top: 0;
     width: 125%;
     aspect-ratio: 3/4;
-    top: 0;
-    left: -12.5%;
+    background-color: rgba(235, 235, 235, 1);
+    border: 1px solid rgba(200, 200, 200, 0.75);
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+      rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    display: none;
   }
-
-  &:hover > main {
-    opacity: 1;
+  &.active > main {
+    display: block;
   }
 
   aside {
@@ -28,13 +27,23 @@ export default styled.article`
     height: 100%;
     background-image: linear-gradient(
         0deg,
-        rgba(0, 0, 0, 0.4) 0%,
+        rgba(0, 0, 0, 0.7) 0%,
         rgba(0, 0, 0, 0) 25%,
         rgba(0, 0, 0, 0) 75%,
-        rgba(0, 0, 0, 0.4) 100%
+        rgba(0, 0, 0, 0.7) 100%
       ),
       url("${(props) => props.rawData.thumb}");
     background-size: cover;
     background-position: center;
+
+    h2 {
+      color: white;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 `;

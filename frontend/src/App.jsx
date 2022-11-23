af0370bf36a82069b1096e92dae19522347b81e0
ValhallaCode <senvisage@gmail.com>
2022-11-23 07:44:06 +0100

    Tribo E06

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import Header from "@components/Header";
 import Navbar from "@components/Navbar";
 import Content from "@components/Content";
-import Styled from "./style";
 import { Routes, Route } from "react-router-dom";
+import Styled from "./style";
 import { Home } from "./pages";
 
 export default function App() {
